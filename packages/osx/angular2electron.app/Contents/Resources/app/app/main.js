27bf7a00a9dd12b3ea640d5c5ba9257cc3d4739c
sercheo87 <chancaysergio@gmail.com>
2016-08-07 10:19:29 -0500

    Correccion de cambios

--- packages/osx/angular2electron.app/Contents/Resources/app/app/main.js
+++ packages/osx/angular2electron.app/Contents/Resources/app/app/main.js
@@ -1,15 +1,27 @@
-System.register("app/main", ["angular2/platform/browser", "app/app"], function($__export) {
+System.register("app/main", ["angular2/platform/browser", "angular2/core", "angular2/router", "app/app"], function($__export) {
   "use strict";
   var bootstrap,
+      provide,
+      ROUTER_PROVIDERS,
+      APP_BASE_HREF,
+      LocationStrategy,
+      HashLocationStrategy,
       Application;
   return {
     setters: [function($__m) {
       bootstrap = $__m.bootstrap;
     }, function($__m) {
+      provide = $__m.provide;
+    }, function($__m) {
+      ROUTER_PROVIDERS = $__m.ROUTER_PROVIDERS;
+      APP_BASE_HREF = $__m.APP_BASE_HREF;
+      LocationStrategy = $__m.LocationStrategy;
+      HashLocationStrategy = $__m.HashLocationStrategy;
+    }, function($__m) {
       Application = $__m.Application;
     }],
     execute: function() {
-      bootstrap(Application);
+      bootstrap(Application, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, {useValue: '/'}), provide(LocationStrategy, {useClass: HashLocationStrategy})]);
     }
   };
 });
